e11ed77582d7a220d1ba56198ff0b45cc6adc801
Karan Jivani <karanjivani@gmail.com>
2016-07-16 17:22:59 -0500

    Chapter 7 finish with out challenges

--- HypnoNerd/ViewController.m
+++ HypnoNerd/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 #import "HypnosisView.h"
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -54,14 +54,48 @@
     textField.placeholder = @"Hypnotize you";
     textField.returnKeyType = UIReturnKeyDone;
     textField.delegate = self;
-    [self.view addSubview:textField];
-    
+    [self.view addSubview:textField];    
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField*)textField {
     NSLog(@"%@",textField.text);
+    
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
     return YES;
 }
 
+-(void)drawHypnoticMessage: (NSString *)message {
+    for (int i = 0; i<20; i++) {
+        UILabel *messageLabel = [[UILabel alloc]init];
+        
+        //configure's the label's color and text
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor redColor];
+        messageLabel.text = message;
+        
+        //This method resizes the label, which will be relative to text that it is displaying
+        [messageLabel sizeToFit];
+        
+        //Get the random x value that fits within the hypnosis view's width
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random()%width;
+        
+        //Get the random y value that fits within the hypnosis view's height
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random()%height;
+        
+        //update the label's frame
+        CGRect frame = messageLabel.frame;
+        frame.origin =  CGPointMake(x, y);
+//        frame.size.height = 30;
+//        frame.size.width = 200;
+        messageLabel.frame = frame;
+        
+        [self.view addSubview:messageLabel];
+        
+    }
+}
 
 @end
